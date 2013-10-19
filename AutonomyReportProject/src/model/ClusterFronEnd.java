e2364a0e055f3872af9aec0a5d33765796929de7
maxRm1970 <massimiliano.polito.rm@gmail.com>
2013-10-19 09:11:42 +0200

    Signed-off-by: maxRm1970 <massimiliano.polito.rm@gmail.com>

--- AutonomyReportProject/src/model/ClusterFronEnd.java
+++ AutonomyReportProject/src/model/ClusterFronEnd.java
@@ -3,6 +3,7 @@ package model;
 
 public class ClusterFronEnd extends HTClusterObject {
 	private boolean linked = true;
+	private String shortName;
 
 	public ClusterFronEnd() {
 		super();
@@ -19,5 +20,14 @@ public class ClusterFronEnd extends HTClusterObject {
 
 	public boolean isLinked() {return linked;}
 	public void setLinked(boolean linked) {this.linked = linked;}
+
+	public String getShortName() {
+		shortName = getNome();
+		if(getNome()!=null && getNome().trim().length()>30) shortName = getNome().substring(0,29)+"...";
+		return shortName;
+	}
+	public void setShortName(String shortName) {this.shortName = shortName;}
+	
+	
 
 }
