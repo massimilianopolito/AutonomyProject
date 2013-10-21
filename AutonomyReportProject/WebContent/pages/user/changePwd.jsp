6228e340a4777dfbc26fef4ff5e43feedc2c2475
maxRm1970 <massimiliano.polito.rm@gmail.com>
2013-10-21 23:43:14 +0200

    Signed-off-by: maxRm1970 <massimiliano.polito.rm@gmail.com>

--- AutonomyReportProject/WebContent/pages/user/changePwd.jsp
+++ AutonomyReportProject/WebContent/pages/user/changePwd.jsp
@@ -1,31 +1,51 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"/>
 
-<head>
-		<link rel="stylesheet" type="text/css" href="<%=request.getContextPath() %>/css/reset.css"/>
-		<link rel="stylesheet" type="text/css" href="<%=request.getContextPath() %>/css/jquery.vegas.css"/>
-		<link rel="stylesheet" type="text/css" href="<%=request.getContextPath() %>/css/global.css"/>
-	
-		<script src="<%=request.getContextPath()%>/js/jquery-1.9.1.min.js" type="text/javascript"></script>
-		<script src="<%=request.getContextPath()%>/js/jquery.vegas.js" type="text/javascript"></script>
-		<script src="<%=request.getContextPath()%>/js/script.js" type="text/javascript"></script>
-	  	<script src="<%=request.getContextPath()%>/js/jquery-ui.custom.js" type="text/javascript"></script>
-
-	
-		<link href='http://fonts.googleapis.com/css?family=Passion+One|Wallpoet|Vast+Shadow|Paytone+One|Jacques+Francois+Shadow|Syncopate|Audiowide' rel='stylesheet' type='text/css'/>
-		<title>Login - D-CUBE | Digital Customer Behaviour</title>
-</head>
-
 <body>
+	<script>
+	$( "#cngPwd" ).click(function() {
+		  var oldPwd = $('#oldPwd').val();
+		  var newPwd = $('#newPwd').val();
+		  
+		  $.ajax({
+			   type: 'POST',
+			   url:'ManageUser',
+			   dataType: 'json',
+			   data: {'oldPwd':oldPwd , 'newPwd':newPwd, 'operation': '2' },
+			   error: function (xhr, ajaxOptions, thrownError){
+		            alert(thrownError);
+		       },   
+			   success: function(res){
+				   var type = res.type;
+				   var msg = res.msg;
+				   var classMsg;
+				   if(type=="0"){
+					   classMsg = "info";
+					   $('#formCngPwd').trigger( "reset" );
+				   }else if(type=="1"){
+					   classMsg = "errore";
+				   }else if(type=="2"){
+					   classMsg = "info";					   
+					   msg = "<B><font style=\"color:#ff0000;\">ATTENZIONE!</font></B> " + msg;
+				   }
+				   $("#msgContainer").removeClass("info").addClass(classMsg);
+				   $('#msg').html(msg);
+			   }
+			 });
+
+		});
+	</script>
+
 	<div id="wrapper">
-		<%@ include file="../header.jsp" %>
+			<div id=msgContainer>
+				<p id="msg"></p>
+			</div>
 			<div class="box boxForm shadow">
 				<div class="title">
 					<p>Compilare la scheda che segue con i valori richiesti</p>
 				</div>
 				<div class="content">
-					<form action="ManageUser" method="post">
-						<input type='hidden' id="operation" name="operation" value="2"/>
+					<form id="formCngPwd" action="ManageUser" method="post">
 						<p>
 						<label>Vecchia Password: </label>
 						<input type="password" id="oldPwd" name="oldPwd"/>
@@ -35,7 +55,7 @@
 						<input type="password" id="newPwd" name="newPwd"/>
 						</p>
 			
-						<input type="submit" class="btnSubmit" value="Cambia"/>
+						<input type="button" id="cngPwd" class="btnSubmit" value="Cambia"/>
 					</form>
 				</div>
 			</div>
--- AutonomyReportProject/src/servlet/ManageUser.java
+++ AutonomyReportProject/src/servlet/ManageUser.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.jk.core.Msg;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import dao.UserDao;
 import model.Message;
@@ -33,40 +35,68 @@ public class ManageUser extends GenericServlet {
 		// TODO Auto-generated method stub
 	}
 	
-	private void changePwd(HttpServletRequest request, String pwd) throws Exception{
+	
+	private JSONObject makeChange(HttpServletRequest request) throws Exception{
+		JSONObject json = new JSONObject();
+		String oldPwd = request.getParameter("oldPwd");
+		String newPwd = request.getParameter("newPwd");
 		String currentUser = request.getRemoteUser();
-		UserDao userDao = new UserDao();
-		userDao.changePwd(currentUser, pwd);
+
+		if(oldPwd==null || oldPwd.isEmpty() || newPwd==null || newPwd.isEmpty()){
+			json.put("type", "2");
+			json.put("msg", "I campi devono essere entrambi correttamente valorizzati.");
+		}else if(oldPwd.equalsIgnoreCase(newPwd)){
+			json.put("type", "2");
+			json.put("msg", "Le due password non possono essere coincidenti.");
+		}else{
+			UserDao userDao = new UserDao();
+			if(userDao.isPwdCorrect(currentUser, oldPwd)){
+				userDao.changePwd(currentUser, newPwd);
+				json.put("type", "0");
+				json.put("msg", "La password e' stata correttamente modificata.");
+			}else{
+				json.put("type", "2");
+				json.put("msg", "La vecchia password non coincide con quella indicata nel sistema.");
+			}
+		}
+		return json;
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String oldPwd = request.getParameter("oldPwd");
-		String newPwd = request.getParameter("newPwd");
 		String operation = request.getParameter("operation");
 		String redirect = "";
 		Message responseMessage = new Message();
 		responseMessage.setType(Message.INFO);
-		//String base ="http://" + request.getServerName() +":" + request.getServerPort() + request.getContextPath() + "/pages/";
+		
+		JSONObject json = new JSONObject();
 
 		try{
 			if("2".equalsIgnoreCase(operation)){
-				changePwd(request, newPwd);
-				redirect = "changePwd.jsp";
-				responseMessage.setText("La password è stata correttamente modificata.");
+				json = makeChange(request);
 			}
 			
 		}catch(Exception e){
 			e.printStackTrace();
-			responseMessage.setText("Si è verificato un errore in fase di modifica password.<br>" + e.getMessage());
+			responseMessage.setText("Si e' verificato un errore <br>" + e.getMessage());
 			responseMessage.setType(Message.ERROR);
+			if("2".equalsIgnoreCase(operation)){
+				json.put("type", "1");
+				json.put("msg", "Si e' verificato un errore in fase di modifica password.<br>" + e.getMessage());
+			}
 		}finally{
 			request.setAttribute("message", responseMessage);
 		}
 
-		forward(request, response, redirect);
+		if("2".equalsIgnoreCase(operation)){
+			request.setCharacterEncoding("UTF8");
+			response.setContentType("application/json; charset=UTF-8");
+			response.getWriter().write(json.toString());
+		}else{
+			forward(request, response, redirect);
+		}
 
 	}
 
